d996d4ce7535da928c03946d8ff8d7a4e11116c4
JacOng17 <JacOng17@users.noreply.github.com>
2026-02-27 10:21:20 +1300

    Update countdownTimer.js for Legacy 2026

--- js/countdownTimer.js
+++ js/countdownTimer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 21, 2026 10:00:00").getTime();
+var countDownDate = new Date("Jul 25, 2026 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdownTimer").innerHTML = "WELCOME TO IMPACT 2026!!!";
+    document.getElementById("countdownTimer").innerHTML = "WELCOME TO LEGACY 2026!!!";
   }
 }, 1000);
